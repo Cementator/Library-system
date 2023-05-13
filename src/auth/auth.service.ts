0a75a09365389848608c81627539a175e2497ac5
Ivica Krancpiler <ikrancpi@gmail.com>
2023-05-13 18:46:36 +0200

    removed user password when user entity is returned on registration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,21 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { IUser, User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
+import { DeepPartial } from 'typeorm';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly userService: UsersService) {}
-  async register(userData: IUser): Promise<User> {
+  async register(userData: IUser): Promise<DeepPartial<User>> {
     const isExistingUser = await this.userService.findOne({
       where: { email: userData.email },
     });
     if (isExistingUser) {
       throw new ConflictException('Account with this email already exists');
     }
-    const user = await this.userService.create(userData);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ...user } = await this.userService.create(userData);
+    console.log(user);
     return user;
   }
 }
